029d13a68c55b99c8b0479fc88eca2001cb51791
Jeevashree412 <jeevashree412@gmail.com>
2025-08-17 00:57:51 +0530

    auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  static final _auth = FirebaseAuth.instance;
+
+  static User? get currentUser => _auth.currentUser;
+
+  static Future<UserCredential> signInAnon() => _auth.signInAnonymously();
+
+  static Future<void> signOut() => _auth.signOut();
+}
